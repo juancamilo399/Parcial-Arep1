7a88b4df74e90119f582249e3fdce52e41ce74b4
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-09-04 12:55:04 -0500

    refactor

--- src/main/java/edu/escuelaing/arep/services/Solve.java
+++ src/main/java/edu/escuelaing/arep/services/Solve.java
@@ -3,6 +3,7 @@ package edu.escuelaing.arep.services;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 
 /**
  * Clase que implementa la logica del servidor
@@ -17,14 +18,22 @@ public class Solve {
     public static String calculate(String data){
         List<Double> nums = new ArrayList<Double>();
         List<String> doubles = Arrays.asList(data.split(","));
-        Double sum=0.0;
+        //Double sum=0.0;
         for (String str : doubles) {
             Double newNum = Double.parseDouble(str);
             nums.add(newNum);
-            sum+=newNum;
+            //sum+=newNum;
         }
-        Double avg=sum/doubles.size();
 
+        //Double avg=sum/doubles.size();
+
+        Double sum = nums.stream()
+                .reduce(0.0, Double::sum);
+
+        OptionalDouble avg1 = nums.stream().mapToDouble(Double::doubleValue).average();
+
+
+        Double avg =avg1.getAsDouble();
         BubbleSort<Double> bubblesorter = new BubbleSort<>();
         Double[] doubles1= nums.toArray(new Double[0]);
         List<Double> result= bubblesorter.bubbleSort(doubles1);
