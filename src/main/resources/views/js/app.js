a4f05557a710f685b2a23f80608470cc40c1edd9
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-09-04 12:24:04 -0500

    fix

--- src/main/resources/views/js/app.js
+++ src/main/resources/views/js/app.js
@@ -5,12 +5,13 @@ button.addEventListener('click', function () {
     var data = document.getElementById('inputnumbers').value;
     console.log(data);
     axios.post('https://gentle-beyond-63920.herokuapp.com/solve/sort/', data)
+    //axios.post('http://localhost:5000/solve/sort/', data)
         .then(res => {
             var result = JSON.parse(res.data)
-            document.getElementById("resultList").innerHTML = "["+ result.List + "]";
+            document.getElementById("orderedList").innerHTML = "["+ result.List + "]";
             document.getElementById("listSum").innerHTML = result.sum ;
             document.getElementById("listAvg").innerHTML = result.avg ;
-            
+
         })
         .catch(function (err) {
             console.log(err);
